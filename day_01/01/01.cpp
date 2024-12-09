3ea4cf2cff415d2d81c9d879fdf6131aea2155a0
AndreySyskov <126235921+AndreySyskov@users.noreply.github.com>
2024-12-09 20:57:28 +0000

    Проверка работы clang - после исправления

--- day_01/01/01.cpp
+++ day_01/01/01.cpp
@@ -1,72 +1,66 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+
 #include "mylib.h"
-
-
 
 using namespace std;
 
 void printArrays(int arr[], int size);
 
+int main(int argc, char* argv[]) {
+  int size = 0;  // счетчик элементов
 
-int main (int argc, char* argv[]){
+  // Имя файла передано вторым аргументом
+  const char* fileName = argv[1];
 
-    
-    int size = 0; // счетчик элементов
+  // Открываем файл для чтения
+  ifstream inputFile(fileName);
+  string line;
+  while (getline(inputFile, line)) {
+    ++size;
+  }
 
-        // Имя файла передано вторым аргументом
-    const char* fileName = argv[1];
+  inputFile.clear();  // Сбрасываем возможные ошибки состояния
+  inputFile.seekg(0, std::ios_base::beg);
 
-    // Открываем файл для чтения
-    ifstream inputFile(fileName);
-    string line;
-     while (getline(inputFile, line)) {
-        ++size;
+  // cout<<size<<endl;
+
+  int arr_1[size], arr_2[size];
+  // printArrays(arr_1, size);
+  // printArrays(arr_2, size);
+
+  // Чтение данных из файла и сохранение их в массив
+  int number;
+  int count = 1;
+  int i = 0;  // счетчик для массива 1
+  int j = 0;  // счетчик для массива 2
+  while (inputFile >> number) {
+    if (count % 2 != 0) {
+      ;
+      arr_1[i] = number;
+      i++;
+    } else {
+      arr_2[j] = number;
+      j++;
     }
-    
-    inputFile.clear();  // Сбрасываем возможные ошибки состояния
-    inputFile.seekg(0, std::ios_base::beg);
+    count++;
+  }
+  // printArrays(arr_1, size);
+  // printArrays(arr_2, size);
+  inputFile.close();
 
-    //cout<<size<<endl; 
-    
-    int arr_1[size], arr_2[size];
-    //printArrays(arr_1, size);
-    //printArrays(arr_2, size);
+  arr_sort(arr_1, size);
 
-    // Чтение данных из файла и сохранение их в массив
-    int number;
-    int count = 1;
-    int i = 0; // счетчик для массива 1
-    int j = 0; // счетчик для массива 2
-    while (inputFile >> number) {
+  arr_sort(arr_2, size);
 
-     
-        if (count%2!=0){
-            ;
-        arr_1[i] = number;
-        i++;}
-        else{
-            
-        arr_2[j] = number;
-        j++;}
-        count++;
-    }
-    //printArrays(arr_1, size);
-    //printArrays(arr_2, size);
-    inputFile.close();
-    
-    arr_sort(arr_1, size);
-    
-    arr_sort(arr_2, size);
-    
-    compar (arr_1, arr_2, size);
+  compar(arr_1, arr_2, size);
 
-    cout<<"Общее расстояние между списками: "<< summ(arr_1, size);
+  cout << "Общее расстояние между списками: " << summ(arr_1, size);
 }
 
 void printArrays(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
+  for (int i = 0; i < size; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
 }
--- day_01/01/mytest.cpp
+++ day_01/01/mytest.cpp
@@ -1,49 +1,49 @@
 #include <gtest/gtest.h>
+
 #include "mylib.h"
 
-
 TEST(ArraySortTest, BasicSort) {
-    int arr[] = {3, 4, 2, 1, 3, 3};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    arr_sort(arr, size);
-    EXPECT_EQ(arr[0], 1);
-    EXPECT_EQ(arr[1], 2);
-    EXPECT_EQ(arr[2], 3);
-    EXPECT_EQ(arr[3], 3);
-    EXPECT_EQ(arr[4], 3);
-    EXPECT_EQ(arr[5], 4);
+  int arr[] = {3, 4, 2, 1, 3, 3};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  arr_sort(arr, size);
+  EXPECT_EQ(arr[0], 1);
+  EXPECT_EQ(arr[1], 2);
+  EXPECT_EQ(arr[2], 3);
+  EXPECT_EQ(arr[3], 3);
+  EXPECT_EQ(arr[4], 3);
+  EXPECT_EQ(arr[5], 4);
 
-    int arr_2[] = {4, 3, 5, 3, 9, 3};
-    
-    arr_sort(arr_2, size);
-    EXPECT_EQ(arr_2[0], 3);
-    EXPECT_EQ(arr_2[1], 3);
-    EXPECT_EQ(arr_2[2], 3);
-    EXPECT_EQ(arr_2[3], 4);
-    EXPECT_EQ(arr_2[4], 5);
-    EXPECT_EQ(arr_2[5], 9);
+  int arr_2[] = {4, 3, 5, 3, 9, 3};
+
+  arr_sort(arr_2, size);
+  EXPECT_EQ(arr_2[0], 3);
+  EXPECT_EQ(arr_2[1], 3);
+  EXPECT_EQ(arr_2[2], 3);
+  EXPECT_EQ(arr_2[3], 4);
+  EXPECT_EQ(arr_2[4], 5);
+  EXPECT_EQ(arr_2[5], 9);
 }
 
 TEST(ComparTest, BasicCompar) {
-    int arr1[] = {1, 2, 3, 3, 3, 4};
-    int arr2[] = {3, 3, 3, 4, 5, 9};
-    int size = sizeof(arr1) / sizeof(arr1[0]);
-    compar(arr1, arr2, size);
-    EXPECT_EQ(arr1[0], 2);
-    EXPECT_EQ(arr1[1], 1);
-    EXPECT_EQ(arr1[2], 0);
-    EXPECT_EQ(arr1[3], 1);
-    EXPECT_EQ(arr1[4], 2);
-    EXPECT_EQ(arr1[5], 5);
+  int arr1[] = {1, 2, 3, 3, 3, 4};
+  int arr2[] = {3, 3, 3, 4, 5, 9};
+  int size = sizeof(arr1) / sizeof(arr1[0]);
+  compar(arr1, arr2, size);
+  EXPECT_EQ(arr1[0], 2);
+  EXPECT_EQ(arr1[1], 1);
+  EXPECT_EQ(arr1[2], 0);
+  EXPECT_EQ(arr1[3], 1);
+  EXPECT_EQ(arr1[4], 2);
+  EXPECT_EQ(arr1[5], 5);
 }
 
 TEST(SummTest, BasicSumm) {
-    int arr[] = {2, 1, 0, 1, 2, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    EXPECT_EQ(summ(arr, size), 11);
+  int arr[] = {2, 1, 0, 1, 2, 5};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  EXPECT_EQ(summ(arr, size), 11);
 }
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }

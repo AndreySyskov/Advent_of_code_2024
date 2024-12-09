1932341be4fe048571427f419bd37f662b5e244e
AndreySyskov <126235921+AndreySyskov@users.noreply.github.com>
2024-12-09 22:01:18 +0000

    Починка теста на clang

--- day_01/01/mylib.cpp
+++ day_01/01/mylib.cpp
@@ -4,7 +4,7 @@
 
 void arr_sort(int* arr, int size) {
   for (int i = 0; i < size - 1; i++) {
-    for (int j = 0; j < size - i - 1; j++) {
+       for (int j = 0; j < size - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         // Меняем arr[j] и arr[j+1]
         int temp = arr[j];
